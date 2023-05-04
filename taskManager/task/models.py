9f685495439a0cbe8ab6ab251a12330bf92a5c12
aashishg07 <ash.gtm07@gmail.com>
2023-05-04 12:20:45 +0545

    first commit

---
+++ taskManager/task/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
